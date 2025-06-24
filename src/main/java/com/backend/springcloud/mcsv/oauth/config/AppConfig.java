2e6053f6f1f3b226e6c1596f03c438e8abd30298
CasseliLayza <casseli.layza@gmail.com>
2025-06-23 23:19:13 -0500

    feat: tracing

--- src/main/java/com/backend/springcloud/mcsv/oauth/config/AppConfig.java
+++ src/main/java/com/backend/springcloud/mcsv/oauth/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.backend.springcloud.mcsv.oauth.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,10 +11,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class AppConfig {
 
-    @Bean
+    /*@Bean
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder().baseUrl("http://USERS-MICROSERVICES-SPRING-CLOUD");
+    }*/
+
+    @Bean
+    WebClient webClient(WebClient.Builder webClientBuilder, ReactorLoadBalancerExchangeFilterFunction lbFunction) {
+        return webClientBuilder.baseUrl("http://USERS-MICROSERVICES-SPRING-CLOUD").filter(lbFunction).build();
     }
 
     @Bean
--- src/main/java/com/backend/springcloud/mcsv/oauth/service/UserService.java
+++ src/main/java/com/backend/springcloud/mcsv/oauth/service/UserService.java
@@ -1,6 +1,8 @@
 package com.backend.springcloud.mcsv.oauth.service;
 
 import com.backend.springcloud.mcsv.oauth.model.User;
+import io.micrometer.tracing.Tracer;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.GrantedAuthority;
@@ -20,17 +22,23 @@ import java.util.stream.Collectors;
 @Service
 public class UserService implements UserDetailsService {
 
+    private final Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
+
     @Autowired
-    private WebClient.Builder webClientBuilder;
+    //private WebClient.Builder webClientBuilder;
+    private WebClient webClient;
+    @Autowired
+    private Tracer tracer;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
+        logger.info("llamada al metodo del service UserService::loadUserByUsername {}", username);
         Map<String, String> params = new HashMap<>();
         params.put("username", username);
 
         try {
-            User user = webClientBuilder.build().get().uri("/users/find/username/{username}", params)
+            //User user = webClientBuilder.build().get().uri("/users/find/username/{username}", params)
+            User user = webClient.get().uri("/users/find/username/{username}", params)
                     .accept(MediaType.APPLICATION_JSON)
                     .retrieve()
                     .bodyToMono(User.class)
@@ -40,11 +48,17 @@ public class UserService implements UserDetailsService {
                     .map(role -> new SimpleGrantedAuthority(role.getName()))
                     .collect(Collectors.toList());
 
+            logger.info("Se ha iniciado session con exito con el username: {} ", user);
+            tracer.currentSpan().tag("success login with user.username: ", user.getUsername());
             return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.isEnable(),
                     true, true, true, roles);
 
         } catch (WebClientException e) {
-            throw new UsernameNotFoundException("User not found - error on login'" + username + "'No existe en el sistema");
+            //"User not found - error on login'" + username + "'No existe en el sistema"
+            String errorMessage = String.format("Error al buscar el usuario %s: %s", username, e.getMessage());
+            logger.error(errorMessage);
+            tracer.currentSpan().tag("error.message", errorMessage + " : " + e.getMessage());
+            throw new UsernameNotFoundException(errorMessage);
         }
     }
 }
